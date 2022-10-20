056bf20c0cb80053eaccd8694eac5d1182de152c
CHENHUI <1366420642@qq.com>
2022-10-20 16:26:56 +0800

    Finetune Code and Add Comments

--- utils.py
+++ utils.py
@@ -12,4 +12,4 @@ import torch.utils.data
 def gather(consts: torch.Tensor, t: torch.Tensor):
     """Gather consts for $t$ and reshape to feature map shape"""
     c = consts.gather(-1, t)
-    return c.reshape(-1, 1, 1, 1)
+    return c.reshape(-1, 1, 1, 1) # (alpha_bar ,1,1,1) broadcast to the ( c ,h ,w )
